66a9db4035a782094c5bc4ff95776ec405dc3f14
Philip <philip26150@gmail.com>
2017-09-29 03:32:47 +0800

    chapter 8 finished

--- article/views.py
+++ article/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Article, Comment
 
 def article(request):
-    context = {}
+    itemList = []
+    for article in Article.objects.all():
+        items = [article]
+        items.extend(list(Comment.objects.filter(article=article)))
+        itemList.append(items)
+    context = {'itemList': itemList}
     return render(request, 'article/article.html', context)
--- populate/admin.py
+++ populate/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 def populate():
     print('Creating admin accounts ... ', end='')
-    # User.objects.all().delete()
+    User.objects.all().delete()
     User.objects.create_superuser(username='admin', password='admin', email=None)
     print('done')
 if __name__ == '__main__':
